b8dd498ab43d8c323686acfd071fb57e4a78f5e4
eslam3bed <iah-93@hotmail.com>
2017-08-16 02:42:27 +0300

    encapsolate orderList

--- src/functions.js
+++ src/functions.js
@@ -16,9 +16,37 @@ const checkMemberCredits = (email, cb) => {
   //closing the pool after finshing
 
 }
+const orderListObject=(orderListId, cb)=>{
+  let orderlistObj={};
+  getOrderList(orderListId , (err , res)=>{
+    if(err){
+      cb(err);
+    }else{
+        orderlistObj.state=res[0].state;
+        orderlistObj.total=res[0].total;
+
+        getDeleverManName(res[0].dman_id ,(err , name)=>{
+          if(err){
+            cb(err)
+          }else{
+            orderlistObj.Dman=name[0].name;
+            getOrders(orderListId , (err ,olist)=>{
+              if(err){
+                cb(err)
+              }else{
+                orderlistObj.orders=olist;
+                cb(orderlistObj);
+              }
+            })
+          }
+        })
+    }
+  })
+}
 const getOrderList=(id , cb)=>{
   const sql = {
-    text: 'SELECT state , total , name as member ,type, price ,dman_id  FROM order_pp join order_list ON order_pp.orderlist = order_list.id JOIN members ON members.id = order_pp.user_id  where order_list.id=$1',
+    //SELECT state , total , name as member ,type, price ,dman_id  FROM order_pp join order_list ON order_pp.orderlist = order_list.id JOIN members ON members.id = order_pp.user_id  where order_list.id
+    text: 'SELECT * FROM order_list WHERE id=$1',
     values: [id]
   }
   client.query(sql, (err, res) => {
@@ -29,18 +57,33 @@ const getOrderList=(id , cb)=>{
     }
   })
 }
-const orderListObject=(res , cb)=>{
-  // let result= {
-  //   state: res.filter((one)=> return one.state),
-  //   total:res.filter((one)=> return one.total) ,
-  //   DelvMan_id:
-  //   orders : res,
-  //
-  // };
-  //
-  // cb(result)
+const getOrders=(orderListId , cb)=>{
+  const sql = {
+    text: 'SELECT * FROM order_pp WHERE orderlist=$1',
+    values: [orderListId]
+  }
+  client.query(sql, (err, res) => {
+    if (err) {
+      cb(err)
+    } else {
+      cb(null, res.rows)
+    }
+  })
+}
+const getDeleverManName=(DmanId ,cb)=>{
+  const sql = {
+    text: 'SELECT * FROM members WHERE id=$1',
+    values: [DmanId]
+  }
+  client.query(sql, (err, res) => {
+    if (err) {
+      cb(err)
+    } else {
+      cb(null, res.rows)
+    }
+  })
 }
 module.exports = {
   checkMemberCredits: checkMemberCredits,
-  getOrderList: getOrderList
+  orderListObject: orderListObject
 }
