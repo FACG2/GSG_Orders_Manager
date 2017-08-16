25c23b5d35d04ec0d3632d50ebc7bbee44003c92
Qamar-93 <youremail@domain.com>
2017-08-16 10:06:36 +0300

    some modifications

--- database/db_connection.js
+++ database/db_connection.js
@@ -1,6 +1,20 @@
 const { Pool } = require ('pg');
 
-require(env2)('../config.env');
+const env = require('env2')
+env('./config.env');
+
+
+switch (process.env.NODE_ENV) {
+  case 'production':
+        process.env.DB_URL= 'postgres://nmphnbtwuldxqs:3cd51aa8da6b43aba9314558df0fffea2f9cac415c3aa9d576d0357782d7494a@ec2-46-137-97-169.eu-west-1.compute.amazonaws.com:5432/d7l1vmkjbnpp5d'
+    break;
+  case 'test':
+       process.env.DB_URL = ''
+  default:
+
+}
+
+
 
 if (!process.env.DB_URL) {
   throw new Error ('No database url provided')
