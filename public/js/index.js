10d0790873071ed01203f94d6138919a1e047a67
mhd <mhd@Hanis-MacBook-Pro.local>
2017-08-17 11:04:20 +0300

    add public files

--- public/js/index.js
+++ public/js/index.js
@@ -1,37 +1,89 @@
 (function(){
-  //{state:true , total : 2 , Dman:'Mahmoud' , orders:[ order ,order ,order ... order ] , error=true , error_message: 'Wrong User Name'}
-document.getElementById('login').addEventListner('click',(event)=>{
-  var x = document.getElementById("usersName");
-  const body={
-    email:document.getElementById('email').value,
-    name:x.options[x.selectedIndex].textContent
-  }
-  checklogin(body , function(err , orderList){
-    if(err){
-      console.log('error in connection');
-    }else{
-      renderOrderList(orderList);
+  //{state:true ,orderlist_id:33 , loginState:true, total : 2 , Dman:'Mahmoud' , orders:[ order ,order ,order ... order ] , error=true , error_message: 'Wrong User Name'}
+
+
+
+document.getElementById('login').addEventListener('click', (event) => {
+
+  // expected data shap;
+  // responce = { login : true ,
+    //resBody: { } ,
+    //message: 'Successfull login' }
+    var x = document.getElementById("usersName");
+    const name_email={
+      email:document.getElementById('email').value,
+      name:x.options[x.selectedIndex].textContent
     }
+
+    login(name_email ,(err, responce) => {
+      if (err) {
+        //handle the error - Print it to the user
+        errorHandler(err, 'login state');
+      } else {
+        //dom the data
+        responce = JSON.parse(responce);
+        if(responce.loginState === true){
+          //on Login Success
+          renderOrderList(responce);
+        }else{
+          //on Login Fails
+          loginError(err ,responce.error_message);
+        }
+
+      }
     })
-
-})
-
-
-function renderOrderList(orderList){
-  //update the state
-  //Delevery man
-  //update the error state
-  //update the order list
-
-  //orders-list
-  var orderL=document.getElementById('orders-list');
-  var tr=document.createElement('tr');
-  var td=document.createElement('td');
-
-  orderList.orders.map((order)=>{
-    tr
   })
+
+
+function loginError(err , message){
+  var login_state=document.getElementById('loginState');
+  login_state.textContent=message;
 }
+
+function renderOrderList(responce){
+    var state=document.getElementById('oState');
+    var DeleveryMan=document.getElementById('DMan');
+    var ordersTable=document.getElementById('ordersTable');
+    state.textContent=responce.resBody.state ? 'Active' : 'Finshed';
+    DeleveryMan.textContent=responce.resBody.Dman;
+
+    var total = 0 ;
+    var NameRow=document.createElement('tr');
+    var TypeRow=document.createElement('tr');
+    var PriceRow=document.createElement('tr');
+    var totalval= document.createElement('span');
+
+    responce.resBody.orders.forEach(function(order){
+        NameRow.textContent = order.name;
+        TypeRow.textContent = order.type;
+        PriceRow.textContent = order.price;
+        total += order.price;
+        totalval.textContent = total;
+        ordersTable.appendChild(NameRow);
+        ordersTable.appendChild(TypeRow);
+        ordersTable.appendChild(PriceRow);
+        ordersTable.appendChild(totalval);
+  });
+
+}
+function errorHandler(err, mag) {
+  alert('error from index.js' + err + mag)
+}
+
+// //check state every 5 minutes
+// setInterval(function(){
+// checkState(function(err ,state){
+//   if(err){
+//     console.log('Something went Wrong!')
+//   }else{
+//     updateState(state);
+//   }
+// })
+// } , 5000);
+
+// function updateState(state){
+//   document.getElementById('oState').textContent =state ? 'Active' : 'Finshed';
+// }
 
 
 })()
--- public/js/request.js
+++ public/js/request.js
@@ -7,3 +7,49 @@
 // 1- request('/login')= >checklogin => return orderList if login success or  return User Not Found Message
 // {state:true , total : 2 , Dman:'Mahmoud' , orders:[ order ,order ,order ... order ]}
 //2- request('/newOrder') => {state : true} or { state : false}
+
+//login
+// checkState()
+//addOrder()
+
+
+function login(credits , fitcher){
+  // creditd = { name: 'any name' , email: 'aya email'}
+  var url='./login';
+  var payLoad = JSON.stringify(credits);
+  _request(url , payLoad ,function(err , data){
+    if(err){
+      fitcher(err)
+    }else{
+      fitcher(null , data);
+    }
+  })
+}
+
+function addOrder(orderDetails , fitcher){
+  // creditd = { name: 'any name' , email: 'aya email'}
+  var url='./newOrder';
+  var payLoad = JSON.stringify(orderDetails);
+  _request(url , payLoad ,function(err , data){
+    if(err){
+      fitcher(err)
+    }else{
+      fitcher(null , data);
+    }
+  })
+}
+
+
+function  _request(url ,payLoad ,comingData){
+  const xhr= new XMLHttpRequest();
+  xhr.onreadystatechange =function(){
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        var data = JSON.parse(xhr.responseText)
+        comingData(null ,data);
+      }else {
+        comingData(xhr.status);
+      }
+      }
+       xhr.open('POST',url,true)
+       xhr.send(payLoad);
+     }
